466bf62faaa9bf11232d7e13b4852e2115eb9b7c
Raen Reyes <raen.reyes11@outlook.com>
2023-11-26 22:39:53 -0800

    for each

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,26 +4,27 @@
     {
         static void Main(string[] args)
         {
-            int[] grades = new int[5];
 
-            grades[0] = 20;
-            grades[1] = 10;
-            grades[2] = 60;
-            grades[3] = 34;
-            grades[4] = 70;
+            int[] nums = new int[10];
 
-            Console.WriteLine("Enter a value to be stored in the array");
-            string input = Console.ReadLine();
-            bool success = int.TryParse(input, out grades[0]);
-            Console.WriteLine("Your value was store in an array and it was " + grades[0]);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("Index " + i + ": ");
+                string input = Console.ReadLine();
+                bool success = int.TryParse(input, out nums[i]);
+            }
+            Console.WriteLine("This is the for loop iterating through array");
+            for (int j = 0;j < nums.Length;j++)
+            {
+                Console.WriteLine("Index " + j + ": " + nums[j]);
+            }
 
-            int[] gradesOfMathA = { 20, 13, 12, 8, 8 };
-
-            int[] gradesOfMathB = { 15, 20, 3, 17, 18, 15 };
-
-            Console.WriteLine("The length of this array is " + gradesOfMathA.Length);
-            Console.WriteLine("The length of this array is " + gradesOfMathB.Length);
-
+            Console.WriteLine("This is the for each loop iterating through array");
+            int counter = 0;
+            foreach (int k in nums) {
+                Console.WriteLine("Index " + counter + ": " + k);
+                counter++;
+            }
         }
     }
 }
