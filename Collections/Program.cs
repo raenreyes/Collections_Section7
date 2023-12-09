b6bda3c32bb0cea231cebfac6e4b95047288ef8e
RaenReyes <raen.reyes@gmail.com>
2023-12-09 08:58:18 -0800

    oop iterate with arrays

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -13,27 +13,13 @@ namespace Collections
         };
         static void Main(string[] args)
         {   
-            //iterate a 2D array with a foreach loop but only used for iteration
-            foreach (int item in matrix) 
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine(" ");
+           
+            _2DArray bruh = new _2DArray();
+            bruh.NormalForLoop(matrix);
+            Console.WriteLine("Below is the For Each loop");
+            bruh.ForEach(matrix);
 
-            //this is how you iterate a 2D array with a for loop
-            Console.WriteLine("This is our 2D array printed using nested for loop");
-            //out for loop
-            for (int i = matrix.GetLength(0) - 1; i >= 0; i--) { 
-                //inner for loop
-                for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
-                {
-                    
-                    Console.Write(matrix[i,j] + " ");
-                }
-                Console.WriteLine(" ");
-            }
+          
         }
-
-
     }
 }
