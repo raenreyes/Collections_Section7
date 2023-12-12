17c9c2da6e73d203a54656c829f40b08dc6d7993
Raen Reyes <raen.reyes11@outlook.com>
2023-12-12 11:38:20 -0800

    bruh

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -5,35 +5,33 @@ namespace Collections
     internal class Program
     {
         static int[,] matrix = {
-
-            {1,2,3 },
-            {4,5,6 },
-            {7,8,9 }
-
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
         };
+
+
         static void Main(string[] args)
         {   
-            //iterate a 2D array with a foreach loop but only used for iteration
-            foreach (int item in matrix) 
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine(" ");
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
-            //this is how you iterate a 2D array with a for loop
-            Console.WriteLine("This is our 2D array printed using nested for loop");
-            //out for loop
-            for (int i = matrix.GetLength(0) - 1; i >= 0; i--) { 
-                //inner for loop
-                for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+            Console.WriteLine("Finding even numbers in a 2D array");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
                 {
-                    
-                    Console.Write(matrix[i,j] + " ");
+                    if (matrix[i, j] % 2 == 0 && matrix[i, j] != matrix[2, 1])
+                    {
+                        Console.Write(matrix[i, j] + ",");
+                    }
+                    else if (matrix[i, j] % 2 == 0)
+                    {
+                        Console.Write(matrix[i, j]);
+                    }
+
                 }
-                Console.WriteLine(" ");
             }
         }
-
-
     }
 }
