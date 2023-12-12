4fdb8fb225a6d14e26d06d16c4fe5aa8a59f147e
Raen Reyes <raen.reyes11@outlook.com>
2023-12-12 11:55:55 -0800

    just messing with iterating over 2d arrays using classes

---
+++ Collections/Interate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Interate
+    {
+        private int[,] _myArray;
+
+        public int[,] Values { get { return _myArray; } set { _myArray = value; } }
+
+        public void ForLoopIterate()
+        {
+            Console.WriteLine("This is the for loop iterating over the 2D array");
+            for (int i = 0; i < _myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < _myArray.GetLength(1); j++)
+                {
+                    Console.Write(_myArray[i, j] + " ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        public void ForEachIterate()
+        {
+            Console.WriteLine("This is the for each loop iterating over the 2D array");
+            foreach (int item in _myArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,34 +4,20 @@ namespace Collections
 {
     internal class Program
     {
-        static int[,] matrix = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
 
 
         static void Main(string[] args)
-        {   
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            Console.WriteLine("Finding even numbers in a 2D array");
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    if (matrix[i, j] % 2 == 0 && matrix[i, j] != matrix[2, 1])
-                    {
-                        Console.Write(matrix[i, j] + ",");
-                    }
-                    else if (matrix[i, j] % 2 == 0)
-                    {
-                        Console.Write(matrix[i, j]);
-                    }
-
-                }
-            }
+        {
+            int[,] arrayOfNumbers = {
+                {34,56,76 },
+                {5,7,8 },
+                {93,46,12 }
+            };
+            Interate bruh = new Interate();
+            bruh.Values = arrayOfNumbers;
+            bruh.ForLoopIterate();
+            Console.WriteLine("------------------------------------------------------------------------------------");
+            bruh.ForEachIterate();
         }
     }
 }
