1dba4ced2d49561db9fa1860330874ce24c1f01c
Raen Reyes <raen.reyes11@outlook.com>
2023-12-20 23:30:25 -0800

    finals are done. Have to crack out more studying. Goal learn dsa then asp.net

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,20 +4,64 @@ namespace Collections
 {
     internal class Program
     {
-
+        static int[,] matrix = {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 4, 7,8 },
+            { 1, 2, 3,4}
+        };
 
         static void Main(string[] args)
         {
-            int[,] arrayOfNumbers = {
-                {34,56,76 },
-                {5,7,8 },
-                {93,46,12 }
-            };
-            Interate bruh = new Interate();
-            bruh.Values = arrayOfNumbers;
-            bruh.ForLoopIterate();
-            Console.WriteLine("------------------------------------------------------------------------------------");
-            bruh.ForEachIterate();
+            Console.WriteLine("Here is the orignal array");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            //first row would be 0,0
+            //second row would be 1,1
+            //third row would be 2,2
+            Console.WriteLine("\nThis will print the diagonals from Top Left To Bottom Right");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+            //first one we want 0,2
+            //seconnd one we want 1,1
+            //last one we want 2,0
+            Console.WriteLine("\nThis will print the diagonals from Top Right To Bottom Left");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i + j == matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
