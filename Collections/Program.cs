0966fde5c9ea9155896563fbc69b40c997b62ece
Raen Reyes <raen.reyes11@outlook.com>
2024-01-03 16:11:31 -0800

    jagged arrays

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,28 +4,39 @@ namespace Collections
 {
     public class Program
     {
-
-
         static void Main(string[] args)
         {
-            char[,] grid =
-            {
-                {'1', '2', '3'},
-                {'4', '5', '6'},
-                {'7', '8', '9'}
+            //declare jagged array
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+            //another way to intialize a jagged array
+            int[][] jaggedArray2 = new int[][] {
+                new int[] {2,3,5,7,11 },
+                new int[] {1,2,3 },
+                new int[] {13,21}
             };
 
-            UserController boardGame = new UserController();
-            Checker validateGame = new Checker();
+            for (int i = 0; i < jaggedArray2.Length; i++)
+            {
+                Console.Write("Array " + (i + 1) + ": ");
+                for (int j = 0; j < jaggedArray2[i].Length; j++)
+                {
+                    Console.Write(jaggedArray2[i][j] + " ");
+                }
 
-            boardGame.MyProperty = grid;
-            boardGame.MyChecker = validateGame;
-            boardGame.StartItUp();
+                Console.WriteLine("");
+            }
+
         }
-
-
-
-
     }
 
 }
