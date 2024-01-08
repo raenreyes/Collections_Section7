db2246550fa3e01ffb50b25c944f93e198d2fa54
Raen Reyes <raen.reyes11@outlook.com>
2024-01-07 22:19:32 -0800

    did two sum using a list

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,49 +1,36 @@
 ﻿using System.Collections;
 using System.Threading.Channels;
+using System.Formats.Asn1;
+using System;
 using System.Collections.Generic;
+
 namespace Collections
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //declare a arraylist with no undefined size of the list
-            ArrayList myArrayList = new ArrayList();
-            //array list with a defined size
-            ArrayList myArrayList2 = new ArrayList(100);
+            List<int> list = new List<int> { 3, 2, 4 };
+            List<int> result = TwoSum(list, 6);
 
-            myArrayList.Add(25);
-            myArrayList.Add("Hello");
-            myArrayList.Add(13.37);
-            myArrayList.Add(5);
-            myArrayList.Add(13);
-            myArrayList.Add(14);
-            myArrayList.Add(25.3f);
-
-            //delete element with specific entry to an array list
-            myArrayList.Remove(13);
-
-
-            //delete element at a specfic position
-            myArrayList.RemoveAt(0);
-
-            //how many objects are in your array list
-            Console.WriteLine("The count of my array list is " + myArrayList.Count);
-
-            double sum = 0;
-
-            foreach (object i in myArrayList)
+            foreach (int k in result)
             {
-                if (i is int || i is double || i is float)
-                {
-                    sum += Convert.ToDouble(i);
-                }
-         
+                Console.Write(k + " ");
             }
-
-            Console.WriteLine("\nThe sum of the arraylist is " + sum);
         }
-
+            public static List<int> TwoSum(List<int> nums, int target)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                for(int j = i + 1; j < nums.Count; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new List<int> { i, j };
+                    }
+                }
+            }
+            return new List<int>(0);
+        }
     }
-
 }
