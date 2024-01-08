51d6a7d41effe28d285c9d0b15d8c741b50157e9
Raen Reyes <raen.reyes11@outlook.com>
2024-01-07 21:31:15 -0800

    quick intro to arraylists and a list

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,33 +1,52 @@
-﻿using System.Threading.Channels;
-
+﻿using System.Collections;
+using System.Threading.Channels;
+using System.Collections.Generic;
 namespace Collections
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            //ArrayList can hold any type of objects like int, string or doubles
+            int num = 6;
 
-            int[] sum1 = new int[] { 1001, 5, 2, 34, -675, 23, 3, 1, 2345, 5 };
-            int[] sum2 = new int[] { 3, 1, 99 };
-            int[] sum3 = new int[] { 58, 2, 89, 23, 344, 345, 65, -6, 53, 4, 34, 35, 5, 65, 65, 34, 3, 5, 4 };
+            string name = "Raen Reyes";
 
-            Console.WriteLine("The min value of the first array is " + Min(sum1));
-            Console.WriteLine("The min value of the first array is " + Min(sum2));
-            Console.WriteLine("The min value of the first array is " + Min(sum3));
+            float pi = 3.14f;
 
-        }
-        public static int Min(params int[] numbers)
-        {
-            int min = int.MaxValue;
-            for (int i = 0; i < numbers.Length; i++)
+            ArrayList myList = new ArrayList();
+            myList.Add(num);
+            myList.Add(name);
+            myList.Add(pi);
+
+            foreach (object i in myList)
             {
-                if (numbers[i] < min)
+                Console.Write(i + " ");
+            }
+
+            //this is a list and its generic which means it can only hold one type of data type
+            List<int> list = new List<int>();
+            int userNum = 0;
+            while (userNum != 99)
+            {
+                Console.WriteLine("Hello pls enter numbers for a list");
+                string toConvert = Console.ReadLine();
+                bool success = int.TryParse(toConvert, out userNum);
+                if(success && userNum != 99)
                 {
-                    min = numbers[i];
+                    list.Add(userNum);
+                }
+                else  {
+                    Console.WriteLine("Pls enter a valid number");
                 }
             }
-            return min;
+            Console.WriteLine("This are the values that you passed");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
         }
+       
     }
 
 }
