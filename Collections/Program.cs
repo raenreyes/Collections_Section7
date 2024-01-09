4a7ddf02b7e0749442ddee84ad79da589a0c3752
Raen Reyes <raen.reyes11@outlook.com>
2024-01-08 22:53:00 -0800

    hashtable challenge iterating over an list to store those indexes into the hashtable

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -14,36 +14,30 @@ namespace Collections
         {
             Hashtable studentsTable = new Hashtable();
 
-            //creating students to put in a hash table
-            Student stud1 = new Student(1, "Maria", 98);
-            Student stud2 = new Student(2, "Jason", 76);
-            Student stud3 = new Student(3, "Clara", 43);
-            Student stud4 = new Student(4, "Steve", 55);
+           
 
-            //store data in hashtable
-            studentsTable.Add(stud1.Id,stud1);
-            studentsTable.Add(stud2.Id,stud2);
-            studentsTable.Add(stud3.Id,stud3);
-            studentsTable.Add(stud4.Id,stud4);
+            List<Student> students = new List<Student>();
 
-            //this will retrive an individual item
-            Student storeStudent1 = (Student)studentsTable[1];
+            students.Add(new Student(1, "Raen", 3.9f));
+            students.Add(new Student(2, "Nicole", 4.5f));
+            students.Add(new Student(3, "Carmen", 3.4f));
+            students.Add(new Student(4, "Victor", 1.9f));
+            students.Add(new Student(1, "Liam", 3.7f));
 
-            //this will retrieve all items from the dictionaryentry way
-            foreach(DictionaryEntry entry in studentsTable)
+            for (int i = 0; i < students.Count; i++)
             {
-                Student temp = (Student)entry.Value;
-
-                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", temp.Id, temp.Name, temp.GPA);
+                if (!studentsTable.ContainsKey(students[i].Id))
+                {
+                    studentsTable.Add(students[i].Id, students[i]);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry this student is already in the table");
+                }
             }
-            Console.WriteLine("\nStudent ID: {0}, Name: {1}, GPA: {2}",storeStudent1.Id,storeStudent1.Name,storeStudent1.GPA);
-
-            //this is a more simplfied version of what we did on getting the data
-            Console.WriteLine("\nSimplified version ");
-            foreach (Student value in studentsTable.Values)
+            foreach (Student item in studentsTable.Values)
             {
-                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", value.Id, value.Name, value.GPA);
-
+                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}",item.Id,item.Name,item.GPA);
             }
         }
 
