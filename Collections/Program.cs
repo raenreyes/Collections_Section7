3a81a03bde773228a134dd922d16e2604ac6baab
Raen Reyes <raen.reyes11@outlook.com>
2024-01-09 21:30:29 -0800

    dictionaries lets go

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -4,6 +4,7 @@ using System.Formats.Asn1;
 using System;
 using System.Collections.Generic;
 
+
 namespace Collections
 {
     public class Program
@@ -12,48 +13,59 @@ namespace Collections
         //Auto - Car
         static void Main(string[] args)
         {
-            Hashtable studentsTable = new Hashtable();
+            Employee[] employees = {
+                new Employee("CEO", "Gwyn", 95, 200),
+                new Employee("Manager", "Joe", 35, 25),
+                new Employee("HR", "Lora", 32, 21),
+                new Employee("Secretary", "Petra", 28, 18),
+                new Employee("Lead Developer", "Raen", 55, 35),
+                new Employee("Intern", "Ernest", 22, 8),
 
+            };
+
+            Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+
+
+            foreach (Employee item in employees)
+            {
+                employeesDirectory.Add(item.Role, item);
+            }
+            //how to fetch a single entry in a dictionary
+            string key = "CEO";
+            if (employeesDirectory.ContainsKey(key))
+            {
+                Employee empl = employeesDirectory["CEO"];
+                Console.WriteLine("This value has been retrived");
+                Console.WriteLine($"Employee name: {empl.Name}, Role: {empl.Role}, Age: {empl.Age}, Salary: ${empl.Salary}");
+            }
+            else {
+                Console.WriteLine("This key does not exist");
+            }
+
+            Employee result = null;
+            //this is the try and get value way of getting data
+            if(employeesDirectory.TryGetValue("Intern", out result))
+            {
+                Console.WriteLine("\nThis value has been retrived");
+                Console.WriteLine($"Employee name: {result.Name}, Role: {result.Role}, Age: {result.Age}, Salary: ${result.Salary}");
+            }
+
+            //using keyvaluepair to iterate over the dictionaary
+            for (int i = 0; i < employeesDirectory.Count; i++) {
+                KeyValuePair<string, Employee> keyValuePair = employeesDirectory.ElementAt(i);
+                Console.WriteLine($"\nKey: {keyValuePair.Key}");
+
+                Employee emplValue = keyValuePair.Value;
+
+                Console.WriteLine($"Employee name: {emplValue.Name}, Role:" +
+                    $" {emplValue.Role}, Age: {emplValue.Age}, Salary: ${emplValue.Salary}");
+            }
            
-
-            List<Student> students = new List<Student>();
-
-            students.Add(new Student(1, "Raen", 3.9f));
-            students.Add(new Student(2, "Nicole", 4.5f));
-            students.Add(new Student(3, "Carmen", 3.4f));
-            students.Add(new Student(4, "Victor", 1.9f));
-            students.Add(new Student(1, "Liam", 3.7f));
-
-            for (int i = 0; i < students.Count; i++)
-            {
-                if (!studentsTable.ContainsKey(students[i].Id))
-                {
-                    studentsTable.Add(students[i].Id, students[i]);
-                }
-                else
-                {
-                    Console.WriteLine("Sorry this student is already in the table");
-                }
-            }
-            foreach (Student item in studentsTable.Values)
-            {
-                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}",item.Id,item.Name,item.GPA);
-            }
         }
 
     }
-    class Student { 
-    
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public float GPA { get; set; }
 
-        public Student(int id, string name, float GPA)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.GPA = GPA;
-        }
-
-    }
 }
+
+
+
